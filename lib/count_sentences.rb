d80ab8544ef657db8487b75a8c99d810d2a6015d
RoyOkite <royokite@gmail.com>
2022-10-24 19:08:44 +0300

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).reject {|s| s.empty?}.count
   end
 end
+
+p "Rokyt.".sentence?
+p "Rokyt?".question?
+p "Rokyt!".exclamation?
+p "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
